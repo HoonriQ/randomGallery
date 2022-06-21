9c40957973f73482e68f39ca394265e8e70f291a
Hoonri <105402441+HoonriQ@users.noreply.github.com>
2022-06-21 11:03:28 +0900

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,7 +1,37 @@
 //이미지 변경 함수
 //배열을 통해 랜덤 이미지 출력
+
+//img_url[0~5]가 이미지의 주소가 되도록 배열 생성
 const img_url = [
-  '../images/img01.jpg',
-  '../images/img02.jpg',
-  '../images/img03.jpg'
+  'images/img01.jpg',
+  'images/img02.jpg',
+  'images/img03.jpg',
+  'images/img04.jpg',
+  'images/img05.jpg',
+  'images/img06.jpg'
 ];
+
+//제이쿼리 선언
+$(function(){
+  
+  //이미지 변경 함수 선언
+  function changeBg(){
+    
+    //0~5랜덤값 생성 (이미지 번호)
+    //0부터 생성하는 이유-배열번호가 0부터 시작이라
+    let imgNum = Math.floor(Math.random()*6);
+    console.log(imgNum);
+    
+    //body의 css속성 변경
+    $('body').css({
+      background: `url(${img_url[imgNum]})`
+    })
+  }
+
+  //setInterval(함수, 시간ms)-시간ms마다 함수가 주기적으로 실행됨
+  setInterval(changeBg, 5000);
+
+})
+
+
+
